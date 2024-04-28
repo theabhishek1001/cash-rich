05dcd2c686f369cfbb9a3c1c46de574481616f61
theabhishek1001 <iabhisheksharma5@gmail.com>
2024-04-28 16:00:20 +0530

    Initial setup of the project

---
+++ coin.manager/coin.manager/src/test/java/com/cash/rich/coin/manager/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cash.rich.coin.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
