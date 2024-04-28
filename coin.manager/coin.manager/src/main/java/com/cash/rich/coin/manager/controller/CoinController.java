56e279a2d60edac5d97bfa97a9467baae18a376d
theabhishek1001 <iabhisheksharma5@gmail.com>
2024-04-29 00:53:04 +0530

    Removed unused imports

--- coin.manager/coin.manager/src/main/java/com/cash/rich/coin/manager/controller/CoinController.java
+++ coin.manager/coin.manager/src/main/java/com/cash/rich/coin/manager/controller/CoinController.java
@@ -6,15 +6,11 @@ import com.cash.rich.coin.manager.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
 import java.util.Optional;
 
 @RestController
